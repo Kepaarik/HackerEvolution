e164905f5c2bd667b44185a495c1c9cfa083633c
Daniil Mishenin <mishenin_2000@mail.ru>
2026-09-24 16:30:34 +0300

    123123

--- HackerEvolution/vite.config.js
+++ HackerEvolution/vite.config.js
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-    plugins: [
-        react(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            includeAssets: ['vite.svg'],
-            manifest: false, // используем готовый public/manifest.json
-            workbox: {
-                globPatterns: ['**/*.{js,css,html,svg,png,woff2,json}'],
-                navigateFallback: '/index.html',
-                runtimeCaching: [
-                    {
-                        urlPattern: ({ request }) => request.destination === 'font',
-                        handler: 'CacheFirst',
-                        options: {
-                            cacheName: 'he-fonts',
-                            expiration: { maxEntries: 20, maxAgeSeconds: 60 * 60 * 24 * 365 },
-                        },
-                    },
-                ],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      manifest: false, // используем готовый public/manifest.json
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,woff2,json}'],
+        navigateFallback: '/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'font',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'he-fonts',
+              expiration: { maxEntries: 20, maxAgeSeconds: 60 * 60 * 24 * 365 },
             },
-        }),
-    ],
-});
+          },
+        ],
+      },
+    }),
+  ],
+})
